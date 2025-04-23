3be69fb409a380b88f39e95afe47ba6f6760b509
Adrian <alfabeto.coradini@hotmail.com>
2025-04-22 22:45:09 -0300

    Finaliza implementacao de criacao de funcionarios

--- employee-list-exercise/src/main/java/com/akiraacs/application/Main.java
+++ employee-list-exercise/src/main/java/com/akiraacs/application/Main.java
@@ -1,7 +1,38 @@
 package com.akiraacs.application;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+import com.akiraacs.entities.Employee;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Locale.setDefault(Locale.US);
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("How many employees will be registered? ");
+        int quantityEmployees = sc.nextInt();
+
+        List<Employee> employees = new ArrayList<>();
+
+        for (int i=0; i<quantityEmployees; i++) {
+            System.out.printf("Employee #%d%n", i+1);
+
+            System.out.print("Id: ");
+            int idEmployee = sc.nextInt();
+            sc.nextLine();
+            System.out.print("Name: ");
+            String nameEmployee = sc.nextLine();
+            System.out.print("Salary: ");
+            double salaryEmployee = sc.nextDouble();
+
+            employees.add(new Employee(idEmployee, nameEmployee, salaryEmployee));
+        }
+
+        System.out.println(employees);
+
+        sc.close();
     }
 }
--- employee-list-exercise/src/main/java/com/akiraacs/entities/Employee.java
+++ employee-list-exercise/src/main/java/com/akiraacs/entities/Employee.java
@@ -1,4 +1,50 @@
 package com.akiraacs.entities;
 
 public class Employee {
+    private int id;
+    private String name;
+    private double salary;
+
+    public Employee(int id, String name, double salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public Employee(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    private void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    private void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    private void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "ID: %d Name: %s Salary: %.2f%n",
+                getId(), getName(), getSalary()
+        );
+    }
 }
